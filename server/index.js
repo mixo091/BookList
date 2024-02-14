7fd134a1408a9c237993bb7cb561b1e54f6ac7ce
mixo091 <mmichopoulos1.1@gmail.com>
2024-02-14 10:47:16 +0200

    Initialized the server-side of the application

---
+++ server/index.js
@@ -0,0 +1,9 @@
+//The standars
+const express = require('express');
+const app = express();
+
+
+//Server starts to listen 
+app.listen(5000 , ()=>{
+    console.log('The server is listening on port 5000..');
+});
