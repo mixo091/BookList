4e339b84bcaef9d865d4f643a0fa672c96270432
mixo091 <mmichopoulos1.1@gmail.com>
2024-02-14 10:51:39 +0200

    updated gitignore

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-//The standars
+//The standars.
 const express = require('express');
 const app = express();
 
 
-//Server starts to listen 
+//Server starts to listen..
 app.listen(5000 , ()=>{
     console.log('The server is listening on port 5000..');
 });
